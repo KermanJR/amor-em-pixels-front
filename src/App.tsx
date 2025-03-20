981edd87d2b73b02b10996e59f8a5fa532d312d8
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2025-03-19 20:35:51 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useParams, useNavigate } from "react-router-dom";
 import Index from "./pages/Index";
 import Precos from "./pages/Precos";
-import CookieConsentBanner from "./components/CookieConsentBanner";
+
 import Create from "./pages/Create";
 import Preview from "./pages/Preview";
 import NotFound from "./pages/NotFound";
@@ -214,7 +214,6 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <CookieConsentBanner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
