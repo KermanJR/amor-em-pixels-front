98b6d60dc0dceafdbc364e8ea70664afe4c48814
Kerman Pereira <63175481+KermanJR@users.noreply.github.com>
2025-03-21 09:30:32 -0400

    Update Create.tsx

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form'
-import { zodResolver } from '@hookform/resolvers/zod';
+import { zodResolver } from '@hookform/resolvers/zod'
 import * as z from 'zod'
 import { Calendar, Heart, Camera, Video, Music, Lock, Loader2, Sparkles } from 'lucide-react';
 import { Button } from '@/components/ui/button';
